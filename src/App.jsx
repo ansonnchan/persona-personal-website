b7741ae74aa351994bae162e87c2a5dff595b7c9
ansonnchan <ananryry180@gmail.com>
2026-04-24 16:18:15 -0700

    Initial release

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route, useLocation, useNavigate } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
-import menuVideo from './assets/menu.mov'
-import main1 from './assets/main1.mp4'
-import main2 from './assets/main2.mp4'
-import main3 from './assets/main3.mp4'
+import menuVideo from './Mainn.mp4'
+import main2 from './main2.mp4'
 import P3Menu from './P3Menu'
 import VideoPage from './VideoPage'
+import ResumePage from './ResumePage'
 import PageTransition from './PageTransition'
+import Socials from './Socials'
+import AboutMe from './AboutMe'
 import './App.css'
 
 function MenuScreen() {
@@ -28,13 +29,13 @@ function AnimatedRoutes() {
           <PageTransition><MenuScreen /></PageTransition>
         } />
         <Route path="/about" element={
-          <PageTransition><VideoPage src={main1} /></PageTransition>
+          <PageTransition variant="about"><AboutMe /></PageTransition>
         } />
         <Route path="/resume" element={
-          <PageTransition><VideoPage src={main2} /></PageTransition>
+          <PageTransition><ResumePage src={main2} /></PageTransition>
         } />
         <Route path="/socials" element={
-          <PageTransition><VideoPage src={main3} /></PageTransition>
+          <PageTransition variant="socials"><Socials /></PageTransition>
         } />
       </Routes>
     </AnimatePresence>
